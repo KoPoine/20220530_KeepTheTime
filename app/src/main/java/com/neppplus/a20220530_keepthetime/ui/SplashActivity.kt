aa6fd799c9dc46a3420e8fc612f74aa472749e7c
김민철 <kmc901206@gmail.com>
2022-05-30 12:06:07 +0900

    스플래시 액티비티에서 자동 로그인 기능 구현

--- app/src/main/java/com/neppplus/a20220530_keepthetime/ui/SplashActivity.kt
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/ui/SplashActivity.kt
@@ -7,9 +7,19 @@ import android.os.Handler
 import android.os.Looper
 import com.neppplus.a20220530_keepthetime.BaseActivity
 import com.neppplus.a20220530_keepthetime.R
+import com.neppplus.a20220530_keepthetime.models.BasicResponse
 import com.neppplus.a20220530_keepthetime.ui.main.LoginActivity
+import com.neppplus.a20220530_keepthetime.ui.main.MainActivity
+import com.neppplus.a20220530_keepthetime.utils.ContextUtil
+import com.neppplus.a20220530_keepthetime.utils.GlobalData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : BaseActivity() {
+
+    var isTokenOk = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -18,7 +28,20 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        apiList.getRequestMyInfo(ContextUtil.getLoginToken(mContext)).enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if (response.isSuccessful) {
+                    val br = response.body()!!
 
+                    isTokenOk = true
+                    GlobalData.loginUser = br.data.user
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+        })
     }
 
     override fun setValues() {
@@ -27,7 +50,13 @@ class SplashActivity : BaseActivity() {
         myHandler.postDelayed({
 
             val myIntent: Intent
-            myIntent = Intent(mContext, LoginActivity::class.java)
+
+            if (isTokenOk && ContextUtil.getAutoLogin(mContext)) {
+                myIntent = Intent(mContext, MainActivity::class.java)
+            }
+            else {
+                myIntent = Intent(mContext, LoginActivity::class.java)
+            }
             startActivity(myIntent)
             finish()
 
