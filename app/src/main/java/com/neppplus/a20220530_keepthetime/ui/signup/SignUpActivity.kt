9fc9168fef74e6f7addd2b9323bf3d606681587a
김민철 <kmc901206@gmail.com>
2022-05-30 10:11:18 +0900

    회원 가입 액티비티 생성

---
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/ui/signup/SignUpActivity.kt
@@ -0,0 +1,28 @@
+package com.neppplus.a20220530_keepthetime.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.a20220530_keepthetime.BaseActivity
+import com.neppplus.a20220530_keepthetime.R
+import com.neppplus.a20220530_keepthetime.databinding.ActivitySignUpBinding
+
+class SignUpActivity : BaseActivity() {
+
+    lateinit var binding : ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
