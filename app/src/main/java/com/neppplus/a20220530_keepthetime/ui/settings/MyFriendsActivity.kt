3d326f057201c30b2844d0a866ccec394aecaca2
김민철 <kmc901206@gmail.com>
2022-05-30 17:47:32 +0900

    프사 변경 이벤트 처리

---
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/ui/settings/MyFriendsActivity.kt
@@ -0,0 +1,12 @@
+package com.neppplus.a20220530_keepthetime.ui.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.neppplus.a20220530_keepthetime.R
+
+class MyFriendsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_friends)
+    }
+}
