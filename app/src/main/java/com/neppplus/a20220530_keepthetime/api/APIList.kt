bdb3b221d6762d3ee6014395ec0442ee3412eaed
김민철 <kmc901206@gmail.com>
2022-05-30 17:14:12 +0900

    프사 변경 이벤트 처리

--- app/src/main/java/com/neppplus/a20220530_keepthetime/api/APIList.kt
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/api/APIList.kt
@@ -1,6 +1,7 @@
 package com.neppplus.a20220530_keepthetime.api
 
 import com.neppplus.a20220530_keepthetime.models.BasicResponse
+import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -37,5 +38,9 @@ interface APIList {
     @Query("type") type : String,
     @Query("value") value : String,
 ) : Call<BasicResponse>
+
+    @Multipart
+    @PUT("/user/image")
+    fun putRequestUserImage(@Part profileImg : MultipartBody.Part) : Call<BasicResponse>
 
 }
