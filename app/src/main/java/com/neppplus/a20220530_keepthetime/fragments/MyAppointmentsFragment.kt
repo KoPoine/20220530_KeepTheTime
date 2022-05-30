222fe689cd96559040fa49ac7ad1b770590a4c4e
김민철 <kmc901206@gmail.com>
2022-05-30 12:28:02 +0900

    ViewPager에 연결한 프래그먼트 3개 생성

---
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/fragments/MyAppointmentsFragment.kt
@@ -0,0 +1,38 @@
+package com.neppplus.a20220530_keepthetime.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.neppplus.a20220530_keepthetime.R
+import com.neppplus.a20220530_keepthetime.databinding.FragmentMyAppointmentsBinding
+
+class MyAppointmentsFragment : BaseFragment() {
+
+    lateinit var binding : FragmentMyAppointmentsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_appointments, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
