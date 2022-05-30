541117b941f09e9f6ed95be59880725ffa8fd22b
김민철 <kmc901206@gmail.com>
2022-05-30 09:58:01 +0900

    스플래시 액티비티 > 로그인 액티비티 연결

--- app/src/main/java/com/neppplus/a20220530_keepthetime/ui/SplashActivity.kt
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/ui/SplashActivity.kt
@@ -1,12 +1,35 @@
 package com.neppplus.a20220530_keepthetime.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.neppplus.a20220530_keepthetime.BaseActivity
 import com.neppplus.a20220530_keepthetime.R
+import com.neppplus.a20220530_keepthetime.ui.main.LoginActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+
+            val myIntent: Intent
+            myIntent = Intent(mContext, LoginActivity::class.java)
+            startActivity(myIntent)
+
+        }, 2500)
     }
 }
