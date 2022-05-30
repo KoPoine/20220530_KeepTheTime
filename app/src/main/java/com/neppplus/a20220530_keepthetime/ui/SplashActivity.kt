b6cbe22683176e268e85c6dc7828bb700823ea22
김민철 <kmc901206@gmail.com>
2022-05-30 09:46:29 +0900

    로그인 액티비티, 스플래시 액티비티 생성

---
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.neppplus.a20220530_keepthetime.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.neppplus.a20220530_keepthetime.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
---
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/ui/main/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.neppplus.a20220530_keepthetime.ui.main
+
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.a20220530_keepthetime.BaseActivity
+import com.neppplus.a20220530_keepthetime.R
+import com.neppplus.a20220530_keepthetime.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity() {
+
+    lateinit var binding : ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
