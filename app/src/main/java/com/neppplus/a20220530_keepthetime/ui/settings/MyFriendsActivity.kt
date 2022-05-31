83fd6bbc20944afcca908bdb90869af3de07e1ba
김민철 <kmc901206@gmail.com>
2022-05-31 09:26:05 +0900

    친구 추가 및 목록을 보여줄 액티비티 생성

--- app/src/main/java/com/neppplus/a20220530_keepthetime/ui/settings/MyFriendsActivity.kt
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/ui/settings/MyFriendsActivity.kt
@@ -2,11 +2,27 @@ package com.neppplus.a20220530_keepthetime.ui.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.a20220530_keepthetime.BaseActivity
 import com.neppplus.a20220530_keepthetime.R
+import com.neppplus.a20220530_keepthetime.databinding.ActivityMyFriendsBinding
 
-class MyFriendsActivity : AppCompatActivity() {
+class MyFriendsActivity : BaseActivity() {
+
+    lateinit var binding : ActivityMyFriendsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_friends)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_my_friends)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
