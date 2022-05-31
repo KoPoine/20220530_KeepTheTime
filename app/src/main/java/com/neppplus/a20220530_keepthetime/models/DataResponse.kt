d1511bd6374287559c4c8cdf2b8b01027ff8e911
김민철 <kmc901206@gmail.com>
2022-05-31 10:13:51 +0900

    친구 검색 Activity xml 작성 완료

--- app/src/main/java/com/neppplus/a20220530_keepthetime/models/DataResponse.kt
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/models/DataResponse.kt
@@ -3,5 +3,6 @@ package com.neppplus.a20220530_keepthetime.models
 class DataResponse (
     val user : UserData,
     val token : String,
+    val users : List<UserData>
         ) {
 }
---
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/ui/settings/AddFriendsActivity.kt
@@ -0,0 +1,12 @@
+package com.neppplus.a20220530_keepthetime.ui.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.neppplus.a20220530_keepthetime.R
+
+class AddFriendsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_friends)
+    }
+}
