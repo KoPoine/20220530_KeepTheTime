f84e8b6150c94159305f5cd5c6bb774baf002dca
김민철 <kmc901206@gmail.com>
2022-05-31 12:54:32 +0900

    닉네임으로 검색하는 API 연결 작성 완료

--- app/src/main/java/com/neppplus/a20220530_keepthetime/adapters/MyFriendsRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/a20220530_keepthetime/adapters/MyFriendsRecyclerAdapter.kt
@@ -5,12 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.databinding.DataBindingUtil
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.neppplus.a20220530_keepthetime.R
-import com.neppplus.a20220530_keepthetime.databinding.ListItemUserBinding
 import com.neppplus.a20220530_keepthetime.models.UserData
 
 class MyFriendsRecyclerAdapter(
@@ -18,26 +18,24 @@ class MyFriendsRecyclerAdapter(
     val mList : List<UserData>
 ) : RecyclerView.Adapter<MyFriendsRecyclerAdapter.ItemViewHolder>() {
 
-    lateinit var binding : ListItemUserBinding
-
     inner class ItemViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+        val profileImg = view.findViewById<ImageView>(R.id.profileImg)
+        val nicknameTxt = view.findViewById<TextView>(R.id.nicknameTxt)
+        val addFriendBtn = view.findViewById<Button>(R.id.addFriendBtn)
+
         fun bind (item : UserData) {
-            Glide.with(mContext).load(item.profileImg).into(binding.profileImg)
-            binding.nicknameTxt.text = item.nickname
-            binding.addFriendBtn.setOnClickListener {
-                Log.d("선택한 목록", item.nickname)
+            Glide.with(mContext).load(item.profileImg).into(profileImg)
+            nicknameTxt.text = item.nickname
+            addFriendBtn.setOnClickListener {
+
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        binding = DataBindingUtil.inflate(
-            LayoutInflater.from(mContext),
-            R.layout.list_item_user,
-            parent,
-            false
-        )
-        return ItemViewHolder(binding.root)
+        val row = LayoutInflater.from(mContext).inflate(R.layout.list_item_user, parent, false)
+        return ItemViewHolder(row)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
